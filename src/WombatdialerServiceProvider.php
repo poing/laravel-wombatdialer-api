65f1dee99b295e5ae38d761a3b4b6153d1dac926
Komal Raja <komal.raja@invite-comm.jp>
2020-10-07 00:08:40 +0900

    Commit with serviceProvider

--- src/WombatdialerServiceProvider.php
+++ src/WombatdialerServiceProvider.php
@@ -1,11 +1,32 @@
 <?php
 
 namespace WombatDialer;
+use Illuminate\Support\ServiceProvider;
 
-class WombatdialerServiceProvider
+class WombatdialerServiceProvider extends ServiceProvider
 {
-  public static function test()
-  {
-    return 'hello world';
-  }
+   /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // Publish Configuration Files
+        $this->publishes([
+            __DIR__ . '/config/wombatdialer.php' => config_path('wombatdialer.php'),
+        ], 'wombatdialer-config');
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // Default Package Configuration
+        $this->mergeConfigFrom(__DIR__ . '/config/wombatdialer.php', 'wombatdialer');
+        $this->mergeConfigFrom(__DIR__ . '/config/default.php', 'wombatdialer');
+    }
 }
