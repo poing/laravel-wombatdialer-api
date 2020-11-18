a085fb51eef7b66d303c84d69c881865b8674743
komalraja <komal.raja@invite-comm.jp>
2020-11-18 16:48:01 +0900

    Uploaded LesserAPI's

---
+++ test/RecallInfoTest.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace WombatDialer\Test;
+use WombatDialer\Controllers\Edit\Wombat;
+use Illuminate\Support\Facades\Http;
+
+class RecallInfoTest extends UnitAbstract
+{
+
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+
+      public function testRecall()
+      {
+     
+       //test RecallInfo()
+        $info = new \WombatDialer\Controllers\RecallInfo;
+       
+        $show = $info->recall();
+        $this->assertIsArray($show, 'The result is not an array');
+        $this->assertArrayHasKey('reschedule_late_limit_sec', $show, 'The Key is not present in an array');
+        
+ }
+ 
+}
+
